f7a6374bb3fdbe349d62df0cf32394c30f6b9ad7
Eneas U de Queiroz <cotequeiroz@gmail.com>
2024-02-09 14:43:48 -0300

    hostapd: add patch for disabling automatic bridging of vlan interfaces
    netifd is responsible for handling that, except if the vlan bridge
    was provided by the config

--- hostapd/config_file.c
+++ hostapd/config_file.c
@@ -3497,6 +3497,8 @@ static int hostapd_config_fill(struct hostapd_config *conf,
 #ifndef CONFIG_NO_VLAN
 	} else if (os_strcmp(buf, "dynamic_vlan") == 0) {
 		bss->ssid.dynamic_vlan = atoi(pos);
+	} else if (os_strcmp(buf, "vlan_no_bridge") == 0) {
+		bss->ssid.vlan_no_bridge = atoi(pos);
 	} else if (os_strcmp(buf, "per_sta_vif") == 0) {
 		bss->ssid.per_sta_vif = atoi(pos);
 	} else if (os_strcmp(buf, "vlan_file") == 0) {
