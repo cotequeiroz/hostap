38a3d681132061fc6a27fa00f258e76c4879d249
Eneas U de Queiroz <cotequeiroz@gmail.com>
2024-02-12 14:18:24 -0300

    Move definition of WLAN_SUPP_RATES_MAX to defs.h
    Patch 460-wpa_supplicant-add-new-config-params-to-be-used-with.patch
    ("wpa_supplicant: add new config params to be used with the ibss join
    command") adds the definition of unsigned char
    rates[WLAN_SUPP_RATES_MAX] to driver.h, which needs to have
    WLAN_SUPP_RATES_MAX defined.  So it includes sta_info.h to get the
    definition.
    
    Commit c74739250a ("AP MLD: Use a helper function to check if a STA is a
    non-AP MLD") makes sta_info.h include driver.h before
    it defines WLAN_SUPP_RATES_MAX, causing an error:
    
    src/drivers/driver.h:969:29: error: 'WLAN_SUPP_RATES_MAX' undeclared here (not in a function)
    
    Move the definition of WLAN_SUPP_RATES_MAX to defs.h to ensure it gets
    defined before other headers are included.  The inclusion of sta_info.h
    in driver.h can be reverted as well.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- src/ap/sta_info.h
+++ src/ap/sta_info.h
@@ -49,10 +49,6 @@
 #define WLAN_STA_PENDING_DEAUTH_CB BIT(30)
 #define WLAN_STA_NONERP BIT(31)
 
-/* Maximum number of supported rates (from both Supported Rates and Extended
- * Supported Rates IEs). */
-#define WLAN_SUPP_RATES_MAX 32
-
 struct hostapd_data;
 
 struct mbo_non_pref_chan_info {
